9a730bde655bc90369fe31c4257c56ec5f02be7d
ViktorKryvorotov <113522935+ViktorKryvorotov@users.noreply.github.com>
2023-02-23 15:59:38 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const setGallery = document.querySelector('.gallery');
+const galleryMarkup = creatGalleryElMarkup(galleryItems);
+setGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+setGallery.addEventListener('click', onSetGalleryClick);
+var lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt', captionDelay:250,});
+
+
+function creatGalleryElMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+      return`
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+        </a>`
+    }).join('');
+  
+}
+
+function onSetGalleryClick(event) {
+     if (event.target.nodeName !== "IMG") {
+    return;
+  }
+    event.preventDefault();
+   
+}
