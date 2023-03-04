bfe18ad508c6d98d0c406b912fdd9ad3fe3dde3f
ViktorKryvorotov <113522935+ViktorKryvorotov@users.noreply.github.com>
2023-03-04 14:59:26 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
+
+
+const feedbackForm = document.querySelector(".feedback-form");
+const emailInputRef = document.querySelector('input[name="email"]');
+const messageInputRef = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const saveData = localStorage.getItem(LOCALSTORAGE_KEY);
+
+
+feedbackForm.addEventListener('input', throttle(handleFormInput, 500));
+function handleFormInput() {
+    const formData = {
+        email: emailInputRef.value,
+        message: messageInputRef.value
+    };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+};
+
+
+if (saveData) {
+    const formData = JSON.parse(saveData);
+    emailInputRef.value = formData.email;
+    messageInputRef.value = formData.message;
+};
+
+
+feedbackForm.addEventListener('submit', handleSubmit);
+function handleSubmit(evt) {
+    evt.preventDefault();
+    const formData = {
+        email: emailInputRef.value,
+        message: messageInputRef.value,
+    };
+    console.log(formData);
+emailInputRef.value = '';
+messageInputRef.value = '';
+localStorage.removeItem(LOCALSTORAGE_KEY);
+};
+
+
+
 
